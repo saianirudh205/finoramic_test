ff73bff80beff6d2252d2a4720072e727ff51462
Sai Anirudh <62623048+saianirudh205@users.noreply.github.com>
2022-01-29 22:15:08 +0530

    Update braces.py

--- braces.py
+++ braces.py
@@ -1,29 +1,24 @@
 
-def braces(A):
-   
-   sign={'+','-','/'}
-   dic={}
-   for i in range(len(A)):
-      if A[i]==')':   
-         x=1
-         j=i-1
-         s=True
-         while j>-1:
-            if s and A[j] in sign:
-               s=False
-            if A[j]=='(':
-               x-=1
-            elif A[j]==')':
-               j=dic[j]
-           
-            if x==0:
-               if s:
-                  return 1
-               dic[i]=j
-               break
-            j-=1
-   
-   return 0
-
-s=input()
-print(braces(s))
+class Solution:
+	# @param A : string
+	# @return an integer
+	def braces(self, A):
+        sign={'+','-','/','*'}
+        dic={}
+        for i in range(len(A)):
+            if A[i]==')':   
+                j=i-1
+                s=True
+                while j>-1:
+                    if s and A[j] in sign:
+                        s=False  
+                    if A[j]==')':
+                        j=dic[j]
+                    elif A[j]=='(':
+                        if s:
+                            return 1
+                        dic[i]=j
+                        break
+                    j-=1
+        
+        return 0
